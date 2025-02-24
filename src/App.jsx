a743f06c7f3e96d16c855d7f6e627ccbf3ff00ce
princeraval147 <princeraval147@gmail.com>
2025-02-24 18:40:08 +0530

    Route Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import Home from './Components/Home'
 
 const App = () => {
 
   return (
     <>
-      <h1>Couse Zone</h1>
+      {/* <h1>Couse Zone</h1> */}
+      <Home />
     </>
   )
 }
