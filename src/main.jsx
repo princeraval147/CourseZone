b749c8fe13e5c1fc4adbf8d30c8edd2464d4d2ac
princeraval147 <princeraval147@gmail.com>
2025-02-25 18:13:14 +0530

    Clean Code

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import Home from './Components/Home.jsx'
-import Error from './Components/Error.jsx'
-import Login from './Components/Login.jsx'
-import ForgetPassword from './Components/ForgetPassword.jsx'
-import Register from './Components/Register.jsx'
-import UpdateProfile from './Components/UpdateProfile.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { AuthProvider } from "./Components/context/AuthContext";
+import { ToastContainer } from "react-toastify";
+import router from "./Route";
+import "react-toastify/dist/ReactToastify.css";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      <Route path='' element={<Home />} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/forgot-password' element={<ForgetPassword />} />
-      <Route path='/updateProfile' element={<UpdateProfile />} />
-      <Route path='*' element={<Error />} />
-    </Route>
-  )
-)
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-    {/* <App /> */}
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+      <ToastContainer position="top-right" autoClose={3000} />
+    </AuthProvider>
+  </React.StrictMode>
+);
