85c19fba4a7da12d64a779a6f0c9536b4608a2c8
princeraval147 <princeraval147@gmail.com>
2025-02-25 17:05:29 +0530

    Route Added

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,19 @@ import App from './App.jsx'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Home from './Components/Home.jsx'
 import Error from './Components/Error.jsx'
+import Login from './Components/Login.jsx'
+import ForgetPassword from './Components/ForgetPassword.jsx'
+import Register from './Components/Register.jsx'
+import UpdateProfile from './Components/UpdateProfile.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route path='' element={<Home />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/forgot-password' element={<ForgetPassword />} />
+      <Route path='/updateProfile' element={<UpdateProfile />} />
       <Route path='*' element={<Error />} />
     </Route>
   )
