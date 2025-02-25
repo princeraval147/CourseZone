e04ebbed2659caf09b2a179cb0de1581abf8bf62
princeraval147 <princeraval147@gmail.com>
2025-02-25 18:07:11 +0530

    Routes

---
+++ src/Route.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import App from "./App";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import Error from "./Components/Error";
+import Register from "./Components/Register";
+import ForgetPassword from "./Components/ForgetPassword";
+import UpdateProfile from "./Components/UpdateProfile";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<App />}>
+            <Route index element={<Home />} />
+            <Route path='register' element={<Register />} />
+            <Route path='login' element={<Login />} />
+            <Route path='forgot-password' element={<ForgetPassword />} />
+            <Route path='updateProfile' element={<UpdateProfile />} />
+            <Route path='*' element={<Error />} />
+        </Route>
+    )
+)
+
+export default router;
