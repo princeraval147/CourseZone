58d01c5b79d586168a61fde10201bdef3a63a0e2
princeraval147 <princeraval147@gmail.com>
2025-03-05 21:42:08 +0530

    Handle Course Routes

--- src/Route.jsx
+++ src/Route.jsx
@@ -4,9 +4,15 @@ import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Error from "./Pages/Error";
 import Contact from "./Pages/Contact";
+import CourseList from './Pages/CourseList'
+import CourseDetails from './Pages/CourseDetails'
 import Payment from "./Pages/Payment";
 import AdminPanel from './Pages/Admin/AdminPanel'
 import Dashboard from "./Pages/Admin/Dashboard";
+import AddCourse from "./Pages/Admin/AddCourse";
+import UpdateCourse from './Pages/Admin/UpdateCourse'
+import MyCourses from "./Pages/Admin/MyCourse";
+import Student from "./Pages/Admin/Student";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -14,10 +20,16 @@ const router = createBrowserRouter(
             <Route index element={<Home />} />
             <Route path='contact' element={<Contact />} />
             <Route path='login' element={<Login />} />
+            <Route path="/course-list" element={<CourseList />} />
+            <Route path="/course-details/:id" element={<CourseDetails />} />
             <Route path='payment' element={<Payment />} />
             //Admin Panel
+            <Route path="update-course/:id" element={<UpdateCourse />} />
             <Route path="admin" element={<AdminPanel />} >
                 <Route path='dashboard' element={<Dashboard />} />
+                <Route path="add-course" element={<AddCourse />} />
+                <Route path="my-course" element={<MyCourses />} />
+                <Route path="student" element={<Student />} />
             </Route>
 
             <Route path='*' element={<Error />} />
