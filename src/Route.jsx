76caf99347e92e7a61e2562d677c9e1cb0ffe67c
princeraval147 <princeraval147@gmail.com>
2025-03-05 12:26:48 +0530

    Manage Routes

--- src/Route.jsx
+++ src/Route.jsx
@@ -1,11 +1,8 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import App from "./App";
-import Home from "./Components/Home";
+import Home from "./Pages/Home";
 import Login from "./Pages/Login";
-import Error from "./Components/Error";
-import Register from "./Components/Register";
-import ForgetPassword from "./Components/ForgetPassword";
-import VerifyOTP from "./Components/VerifyOTP";
+import Error from "./Pages/Error";
 import Contact from "./Pages/Contact";
 import Payment from "./Pages/Payment";
 import AdminPanel from './Pages/Admin/AdminPanel'
@@ -15,12 +12,9 @@ const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<App />}>
             <Route index element={<Home />} />
-            <Route path='register' element={<Register />} />
+            <Route path='contact' element={<Contact />} />
             <Route path='login' element={<Login />} />
             <Route path='payment' element={<Payment />} />
-            <Route path='forgotPassword' element={<ForgetPassword />} />
-            <Route path='contact' element={<Contact />} />
-            <Route path='VerifyOTP' element={<VerifyOTP />} />
             //Admin Panel
             <Route path="admin" element={<AdminPanel />} >
                 <Route path='dashboard' element={<Dashboard />} />
