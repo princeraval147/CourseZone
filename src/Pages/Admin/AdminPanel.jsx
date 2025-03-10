1d938d25e2a3575333ffcd730b14865804205d8b
princeraval147 <princeraval147@gmail.com>
2025-03-10 11:29:54 +0530

    Protected Route

--- src/Pages/Admin/AdminPanel.jsx
+++ src/Pages/Admin/AdminPanel.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Sidebar from './Sidebar'
 import style from '../../Styles/AdminPanel.module.css'
+import AdminAuth from '../../Components/hooks/useAdminAuth'
 
 const AdminPanel = () => {
+    AdminAuth();
     return (
         <>
             <div className={style.adminPanel}>
