0d818aca7f3b8cbcef7647e9a2ea1d41aad19394
princeraval147 <princeraval147@gmail.com>
2025-03-18 21:36:11 +0530

    userSchema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,15 +1,27 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-  isVerified: { type: Boolean, default: false },
-  otp: String,
-  otpExpires: Date,
-  role:{type:String,enum:["instructor", "student"],default:'student'},
-  enrolledCourses:[{type:mongoose.Schema.Types.ObjectId,ref:'Course'}],
-  photoUrl:{type:String,default:""}
- }, {timestamps:true});
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    isVerified: { type: Boolean, default: false },
+    otp: String,
+    otpExpires: Date,
+    role: {
+      type: String,
+      enum: ["instructor", "student", "pending-instructor"],
+      default: "student",
+    },
+    isInstructorApproved: {
+      type: Boolean,
+      default: false,
+    },
+    enrolledCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
+    savedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
+    photoUrl: { type: String, default: "" },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
