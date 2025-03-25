7ea2b7d9d90b98169b7b50e2e77250d651589238
princeraval147 <princeraval147@gmail.com>
2025-03-25 11:20:15 +0530

    Instructor Profile Done

--- src/Pages/InstructorProfile.jsx
+++ src/Pages/InstructorProfile.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import styles from "../styles/InstructorProfile.module.css";
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 
 const InstructorProfile = () => {
 
     const { id } = useParams();
     const [instructor, setInstructor] = useState(null);
     const [courses, setCourses] = useState([]);
+    const Navigate = useNavigate();
 
     useEffect(() => {
         const fetchInstructorData = async () => {
@@ -29,10 +31,12 @@ const InstructorProfile = () => {
                 console.error("Error fetching instructor data:", error);
             }
         };
-
-
         fetchInstructorData();
     }, [id]);
+
+    const totalCourse = courses.length;
+
+    console.log("courses = ", courses);
 
     if (!instructor) return <p>Loading instructor profile...</p>;
 
@@ -40,8 +44,11 @@ const InstructorProfile = () => {
         <div className={styles.instructorProfileContainer}>
             <div className={styles.instructorInfo}>
                 <img src={instructor.photoUrl} alt={instructor.username} className={styles.avatar} />
-                <h1>{instructor.username}</h1>
-                <p>Email: {instructor.email}</p>
+                <div className={styles.aboutInstructor}>
+                    <h1>{instructor.username}</h1>
+                    <p>Email: {instructor.email}</p>
+                    <span className={styles.totalCourse}><PlayCircleIcon /> {totalCourse} Courses Uploaded </span>
+                </div>
             </div>
 
             <div className={styles.coursesSection}>
@@ -49,7 +56,12 @@ const InstructorProfile = () => {
                 {courses.length > 0 ? (
                     <div className={styles.courseList}>
                         {courses.map((course) => (
-                            <div key={course._id} className={styles.courseCard}>
+                            <div
+                                key={course._id}
+                                className={styles.courseCard}
+                                onClick={() => Navigate(`/course-details/${course._id}`)}
+                                style={{ cursor: "pointer" }}
+                            >
                                 <img src={course.courseImage} alt={course.title} />
                                 <h3>{course.title}</h3>
                                 <p>{course.description.substring(0, 100)}...</p>
