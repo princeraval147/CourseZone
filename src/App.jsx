80c2eeff9c86a2e9b571b87653dcdf2c7c3df8cd
princeraval147 <princeraval147@gmail.com>
2025-03-28 12:40:38 +0530

    Add Theme Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Outlet } from 'react-router-dom'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+import { ThemeProvider } from "./Components/ThemeContext";
 
 const App = () => {
 
   return (
     <>
-      <Header />
-      <Outlet />
-      <Footer />
+      <ThemeProvider>
+        <Header />
+        <Outlet />
+        <Footer />
+      </ThemeProvider>
     </>
   )
 }
