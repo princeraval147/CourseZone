9b18a810d693177545d674c307e0d7538edd7a42
princeraval147 <princeraval147@gmail.com>
2025-04-05 11:58:01 +0530

    Manage instructor

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -11,14 +11,19 @@ const {
     authenticate,
     userrole,
     isLoggedIn,
-    checkCourse,
-    toggleSavedCourse,
     myCourses,
+    savecourse,
     getSavedCourses,
-    instructorrequest
+    instructorrequest,
+    toggleSavedCourse,
+    checkCourse,
+    getallinstrucors,
+    pramoteadmin,
+    demotestudent
 } = require("../controllers/authController");
 const authenticateUser = require("../middleware/authMiddleware");
 const upload = require("../utils/multer");
+const { authenticateToken, isAdmin } = require("../middleware/isAdminMiddleware");
 
 const router = express.Router();
 
@@ -28,9 +33,9 @@ router.post("/login", login);
 router.post("/logout", logout);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
-router.post("/checkCourse", checkCourse);
 router.get("/profile", authenticateUser, getProfile);
 router.get("/authenticate", authenticateUser, authenticate);
+// router.put("/update-profile", authenticateUser, updateProfile); 
 router.put("/update-profile", authenticateUser, upload.single("profilePhoto"), updateProfile);
 router.get("/me", authenticateUser, userrole);
 router.get("/isLoggedIn", authenticateUser, isLoggedIn);
@@ -38,5 +43,9 @@ router.get("/my-courses", authenticateUser, myCourses);
 router.post("/toggle-save", authenticateUser, toggleSavedCourse);
 router.get("/saved-courses", authenticateUser, getSavedCourses);
 router.post("/request", authenticateUser, instructorrequest);
+router.post("/checkCourse", checkCourse);
+router.get("/instructors", authenticateToken, isAdmin, getallinstrucors);
+router.put("/instructors/:id/promote", authenticateToken, isAdmin, pramoteadmin)
+router.put("/instructors/:id/demote", authenticateToken, isAdmin, demotestudent)
 
 module.exports = router;
--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createOrder, verifyPayment, paymentsuccess, getEnrolledStudents, getAllEnrolledStudents } = require("../controllers/razorpayController");
+const { createOrder, verifyPayment, paymentsuccess, getEnrolledStudents, getAllEnrolledStudents, enrolledstudent } = require("../controllers/razorpayController");
 const authMiddleware = require("../middleware/authMiddleware");
 const authenticateAdmin = require("../middleware/adminMiddleware");
 const { isAdmin, authenticateToken } = require("../middleware/isAdminMiddleware");
@@ -9,7 +9,8 @@ const router = express.Router();
 router.post("/create-order", authMiddleware, createOrder);
 router.post("/verify-payment", authMiddleware, verifyPayment);
 router.post("/payment-success", authMiddleware, paymentsuccess);
-router.get("/enrolled-students", authenticateAdmin, getEnrolledStudents);
+// router.get("/enrolled-students", authenticateAdmin, getEnrolledStudents);
 router.get("/allenrolled-students", authenticateToken, isAdmin, getAllEnrolledStudents);
+router.get("/enrolled-students", authenticateAdmin, enrolledstudent);
 
 module.exports = router;
